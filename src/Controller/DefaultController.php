050bf44b4ea0d0fa9d2f35561f6980f687551359
Kettengeschwindigkeit <666damned999@mail.ru>
2024-10-13 20:22:02 +0300

    Creating ManyToOne bind, adding EntityType to form

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use App\Entity\Blog;
 use App\Repository\BlogRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -14,30 +13,6 @@ class DefaultController extends AbstractController
     #[Route('/', name: 'blog_default')]
     public function index(BlogRepository $blogRepository, EntityManagerInterface $em): Response
     {
-        $blogs = $blogRepository->findAll();
-//        $blogsWithId1 = $blogRepository->findBy(['id' => 1]);
-        $blog = $blogRepository->findOneBy(['id' => 3]);
-
-        /** Изменить тайтл */
-//        $blog->setTitle('Title 2');
-
-        /** Удалиь запись */
-//        $em->remove($blog);
-//        $em->flush();
-
-        /** Обновить данные */
-//        $em->refresh($blog);
-
-//        dump($blog);
-
-        $blog = (new Blog())
-            ->setTitle('Title')
-            ->setDescription('Description')
-            ->setText('Text');
-
-        $em->persist($blog);
-        $em->flush();
-
-        return $this->render('default/index.html.twig', []);
+        return $this->redirectToRoute('app_blog_index');
     }
 }
